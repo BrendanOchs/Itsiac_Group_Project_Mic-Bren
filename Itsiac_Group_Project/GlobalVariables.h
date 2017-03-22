baa6e03e9f9b6224c54ba83d8966fd1234d884ea
Micaiah Skolnick <micaiahskolnickdeveloper@gmail.com>
2017-03-22 08:34:56 -0500

    Added Global Variable header file. Started building structure of program

---
+++ Itsiac_Group_Project/GlobalVariables.h
@@ -0,0 +1,11 @@
+//*****************************************************************************************************
+
+//This file is used only to declare global variables so they are all in one location
+
+
+int lineCount, int MAXLINECOUNT;
+int processList[];
+
+
+
+//*****************************************************************************************************
