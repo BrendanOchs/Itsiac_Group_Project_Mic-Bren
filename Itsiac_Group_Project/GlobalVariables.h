d16b7387f95f20cffc2aad719658660be6bad164
Brendan Ochs <brendanochs@gmail.com>
2017-03-22 11:34:38 -0500

    test change

--- Itsiac_Group_Project/GlobalVariables.h
+++ Itsiac_Group_Project/GlobalVariables.h
@@ -3,11 +3,8 @@
 //This file is used only to declare global variables so they are all in one location
 
 
-<<<<<<< HEAD
 int MAXLINECOUNT =50;
-=======
 int lineCount;
->>>>>>> m-Skolnick/master
 int processList[50];
 
 
