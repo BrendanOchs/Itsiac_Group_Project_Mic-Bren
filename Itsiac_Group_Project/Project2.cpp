77ce31b6336767daadd683a0cc94d00a63ed736e
Micaiah Skolnick <micaiahskolnickdeveloper@gmail.com>
2017-04-10 11:06:22 -0500

    Micaiah's change #2

--- Itsiac_Group_Project/Project2.cpp
+++ Itsiac_Group_Project/Project2.cpp
@@ -64,11 +64,8 @@ void Footer(ofstream &Outfile){
 		// Returns - Nothing
 	Outfile << endl;
 	Outfile << setw(35) << "-------------------------------- - " << endl;
-<<<<<<< HEAD
-	Outfile << setw(35) << " | You are still a TWAT | " << endl;
-=======
-	Outfile << setw(35) << " | Micaiah's change | " << endl;
->>>>>>> m-Skolnick/master
+ // Micaiah's change #2
+
 	Outfile << setw(35) << "-------------------------------- - " << endl;
 	lineCount += 4;
 	return;
