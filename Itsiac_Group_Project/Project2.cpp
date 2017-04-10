f0f4b35ee22296fb3f29bf6ff0d03f0c9a6d14a2
Brendan Ochs <brendanochs@gmail.com>
2017-04-10 11:13:27 -0500

    committed your changes

--- Itsiac_Group_Project/Project2.cpp
+++ Itsiac_Group_Project/Project2.cpp
@@ -64,11 +64,7 @@ void Footer(ofstream &Outfile){
 		// Returns - Nothing
 	Outfile << endl;
 	Outfile << setw(35) << "-------------------------------- - " << endl;
-<<<<<<< HEAD
-    // Micaiah's change #2
-=======
  // Micaiah's change #3
->>>>>>> m-Skolnick/master
 	Outfile << setw(35) << "-------------------------------- - " << endl;
 	lineCount += 4;
 	return;
