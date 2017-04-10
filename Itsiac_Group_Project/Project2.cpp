ef2e517f7f3b0ff37c40a40ac6f5b7ee234a1278
Micaiah Skolnick <micaiahskolnickdeveloper@gmail.com>
2017-04-10 10:56:33 -0500

    Micaiah's change

--- Itsiac_Group_Project/Project2.cpp
+++ Itsiac_Group_Project/Project2.cpp
@@ -64,7 +64,7 @@ void Footer(ofstream &Outfile){
 		// Returns - Nothing
 	Outfile << endl;
 	Outfile << setw(35) << "-------------------------------- - " << endl;
-	Outfile << setw(35) << " | END OF PROGRAM OUTPUT | " << endl;
+	Outfile << setw(35) << " | Micaiah's change | " << endl;
 	Outfile << setw(35) << "-------------------------------- - " << endl;
 	lineCount += 4;
 	return;
